ed7ece7da779b45b510a1543dbef43724b570b3a
llm <llm@opioinc.com>
2025-04-01 23:10:42 +1300

    files in wrong project

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,9 +1,6 @@
 pub mod index;
-pub mod plugin;
 pub mod polars_functions;
 pub mod registry;
-pub mod transform;
 
 pub use index::{LookupIndex, Value};
 pub use registry::TableRegistry;
-pub use transform::TransformType;
